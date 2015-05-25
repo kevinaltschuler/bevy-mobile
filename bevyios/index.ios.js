b26517af18f526e5a090ecbf2fcdd9a5faf66391
kevtastic <kevinaltschuler@gmail.com>
2015-05-24 21:28:20 -0400

    first commit

---
+++ bevyios/index.ios.js
@@ -0,0 +1,45 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+var {
+  AppRegistry,
+  StyleSheet,
+  TabBarIOS,
+  Text,
+  View,
+} = React;
+
+var TabBar = require('./js/TabBar.ios.js');
+
+var bevyios = React.createClass({
+  render: function() {
+    return (
+        <TabBar />
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('bevyios', () => bevyios);
