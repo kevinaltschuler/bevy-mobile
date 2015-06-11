277e81544afdb52e2520ea16c948c33843dea1c3
Benjamin Carroll <b3ncarroll@gmail.com>
2015-06-11 17:08:23 -0400

    fixed background

--- bevyios/js/login/components/LoginView.ios.js
+++ bevyios/js/login/components/LoginView.ios.js
@@ -14,7 +14,7 @@ var {
   Image
 } = React;
 
-var backgroundImage = require('image!backgroundImage');
+var backgroundImage = require('image!loginBackground');
 
 var RegisterView = require('./RegisterView.ios.js');
 
@@ -86,6 +86,9 @@ var styles = StyleSheet.create({
   loginBackground: {
     flex: 1,
     resizeMode: 'cover',
+    width: 500,
+    height: 500,
+  },
 
   logo: {
     width: 50,
