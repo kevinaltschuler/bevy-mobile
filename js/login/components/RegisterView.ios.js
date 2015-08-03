ebf106fdf27ba65be9560feabb1000897963e36d
Albert Chen <blahoink@gmail.com>
2015-08-03 15:24:22 -0400

    register works

--- js/login/components/RegisterView.ios.js
+++ js/login/components/RegisterView.ios.js
@@ -6,8 +6,6 @@
 
 // get modules
 var React = require('react-native');
-var backgroundImage = require('image!loginBackground');
-
 var {
   View,
   StyleSheet,
@@ -17,18 +15,64 @@ var {
   Image
 } = React;
 
+var _ = require('underscore');
+var constants = require('./../../constants');
+var UserStore = require('./../../profile/UserStore');
+var AppActions = require('./../../app/AppActions');
+
 var RegisterView = React.createClass({
 
   propTypes: {
     message: React.PropTypes.string,
-    loginNavigator: React.PropTypes.object
+    loginNavigator: React.PropTypes.object,
+    authModalActions: React.PropTypes.object
   },
 
   getInitialState() {
     return {
       email: '',
-      pass: ''
+      pass: '',
+      error: ''
     };
+  },
+
+  onRegister() {
+    fetch(constants.apiurl + '/users', {
+      method: 'post',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: this.state.email,
+        password: this.state.pass
+      })
+    })
+    .then((res) => (res.json()))
+    .then((res) => {
+      if(res.object == undefined) {
+        // success
+        UserStore.setUser(res);
+        AppActions.load();
+
+        this.setState({
+          email: '',
+          pass: '',
+          error: ''
+        });
+        this.props.authModalActions.close();
+      } else {
+        // error
+        this.setState({ error: res.message });
+      }
+    })
+  },
+
+  _renderError() {
+    if(_.isEmpty(this.state.error)) return <View />;
+    return (
+      <Text style={ styles.errorText }>{ this.state.error }</Text>
+    );
   },
 
   render: function() {
@@ -37,6 +81,7 @@ var RegisterView = React.createClass({
         <Text style={styles.registerTitle}>
           Create An Account
         </Text>
+        { this._renderError() }
         <View style={{
           flex: 1,
           height: 1,
@@ -45,9 +90,11 @@ var RegisterView = React.createClass({
         <TextInput
           autoCorrect={ false }
           autoCapitalize='none'
+          keyboardType='email-address'
           placeholder='Email Address'
           placeholderTextColor='#aaa'
           style={ styles.registerInput }
+          value={ this.state.email }
           onChangeText={(text) => this.setState({ email: text })}
         />
         <View style={{
@@ -62,6 +109,7 @@ var RegisterView = React.createClass({
           placeholder='•••••••'
           placeholderTextColor='#aaa'
           style={ styles.registerInput }
+          value={ this.state.pass }
           onChangeText={(text) => this.setState({ pass: text })}
         />
         <View style={{
@@ -72,10 +120,10 @@ var RegisterView = React.createClass({
         }} />
         <TouchableHighlight 
           style={ styles.registerButton }
-          underlayColor="#eee">
-          <Text style={ styles.registerButtonText }>
-            Create
-          </Text>
+          underlayColor='rgba(44,182,105,0.8)'
+          onPress={ this.onRegister }
+        >
+          <Text style={ styles.registerButtonText }>Create</Text>
         </TouchableHighlight>
         <TouchableHighlight
           underlayColor='#eee'
@@ -107,6 +155,12 @@ var styles = StyleSheet.create({
     fontSize: 17,
     color: '#666',
     marginBottom: 10
+  },
+  errorText: {
+    flex: 1,
+    textAlign: 'center',
+    color: '#df4a32',
+    marginBottom: 5
   },
   registerInput: {
     flex: 1,
