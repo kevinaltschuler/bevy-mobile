714f49f97e3b07e29b7f8e5977825bf08e8c5892
Albert Chen <blahoink@gmail.com>
2015-09-25 22:05:18 -0400

    basic thread components

---
+++ js/chat/components/android/ThreadItem.android.js
@@ -0,0 +1,40 @@
+/**
+ * ThreadItem.android.js
+ * @author albert
+ */
+
+'use strict';
+
+var React = require('react-native');
+var {
+  Text,
+  View,
+  StyleSheet
+} = React;
+
+var ThreadItem = React.createClass({
+
+  propTypes: {
+
+  },
+
+  getInitialState() {
+    return {
+
+    }
+  },
+
+  render() {
+    return (
+      <View>
+
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+
+});
+
+module.exports = ThreadItem;
---
+++ js/chat/components/android/ThreadView.android.js
@@ -0,0 +1,30 @@
+/**
+ * ThreadView.android.js
+ * @author albert
+ */
+
+'use strict';
+
+var React = require('react-native');
+var {
+  View,
+  Text,
+  StyleSheet
+} = React;
+
+var ThreadView = React.createClass({
+  render() {
+    return (
+      <View style={ styles.container }>
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+
+  }
+});
+
+module.exports = ThreadView;
