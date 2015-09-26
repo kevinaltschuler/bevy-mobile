6358ff1f162ea891d4c3d421a4823c022273ef99
Albert Chen <blahoink@gmail.com>
2015-09-25 21:36:26 -0400

    basic post list

---
+++ js/post/components/android/PostList.android.js
@@ -0,0 +1,31 @@
+/**
+ * PostList.android.js
+ * @author albert
+ */
+
+'use strict';
+
+var React = require('react-native');
+var {
+  View,
+  Text,
+  StyleSheet
+} = React;
+
+var PostList = React.createClass({
+  render() {
+    return (
+      <View style={ styles.container }>
+        
+      </View>
+    );
+  }
+});
+
+var styles = StyleSheet.create({
+  container: {
+
+  }
+});
+
+module.exports = PostList;
