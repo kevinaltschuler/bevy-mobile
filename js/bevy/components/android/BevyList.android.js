7618fe211aa8adfde83c67d87a475e6ddb7f9fa0
Albert Chen <blahoink@gmail.com>
2015-10-02 13:07:03 -0400

    switch to listview

--- js/bevy/components/android/BevyList.android.js
+++ js/bevy/components/android/BevyList.android.js
@@ -7,7 +7,7 @@
 
 var React = require('react-native');
 var {
-  ScrollView,
+  ListView,
   View,
   Text,
   StyleSheet
@@ -20,25 +20,33 @@ var BevyList = React.createClass({
     myBevies: React.PropTypes.array
   },
 
-  _renderBevies() {
-    var bevies = [];
-    for(var key in this.props.myBevies) {
-      var bevy = this.props.myBevies[key];
-      bevies.push(
-        <BevyListItem
-          key={ 'bevylistitem:' + bevy._id }
-          bevy={ bevy }
-        />
-      );
-    }
-    return bevies;
+  getInitialState() {
+    var bevyData = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    return {
+      bevies: bevyData.cloneWithRows(this.props.myBevies)
+    };
+  },
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      bevies: this.state.bevies.cloneWithRows(nextProps.myBevies)
+    });
   },
 
   render() {
     return (
-      <ScrollView style={ styles.container }>
-        { this._renderBevies() }
-      </ScrollView>
+      <ListView 
+        dataSource={ this.state.bevies }
+        style={ styles.container }
+        renderRow={(bevy) => {
+          return (
+            <BevyListItem
+              key={ 'bevylistitem:' + bevy._id }
+              bevy={ bevy }
+            />
+          );
+        }}
+      />
     );
   }
 });
