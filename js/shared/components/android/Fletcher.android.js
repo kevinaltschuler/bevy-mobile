4ddb24347132400d353687f495ff2c06f12ed665
Albert Chen <blahoink@gmail.com>
2015-10-30 15:19:19 -0400

    fix fletcher for ios

--- js/shared/components/android/Fletcher.android.js
+++ js/shared/components/android/Fletcher.android.js
@@ -4,6 +4,18 @@
 
 'use strict';
 
-var { NativeModules } = require('react-native');
+var React = require('react-native');
+var { 
+  NativeModules,
+  Platform
+} = React;
 
-module.exports = NativeModules.Fletcher;
+var Fletcher = {};
+
+if(Platform.OS == 'android') {
+  Fletcher = NativeModules.Fletcher
+} else {
+  Fletcher = {};
+}
+
+module.exports = Fletcher;
