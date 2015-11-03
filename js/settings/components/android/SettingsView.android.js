f6f7579ef649bd194f3e9db63791ca7c2b357b56
Albert Chen <blahoink@gmail.com>
2015-11-02 19:13:37 -0500

    app version

--- js/settings/components/android/SettingsView.android.js
+++ js/settings/components/android/SettingsView.android.js
@@ -151,16 +151,16 @@ var SettingsView = React.createClass({
           </View>
         </TouchableNativeFeedback>
         <Text style={ styles.settingHeader }>
-          App Settings
+          About
         </Text>
-        <TouchableNativeFeedback
-        >
-            <View style={ styles.settingButton }>
-              <Text style={ styles.settingButtonText }>
-                PlaceholderSetting
-              </Text>
-            </View>
-        </TouchableNativeFeedback>
+        <View style={ styles.settingButton }>
+          <Text style={ styles.settingButtonText }>
+            Version
+          </Text>
+          <Text style={ styles.settingButtonTextRight }>
+            0.0.2
+          </Text>
+        </View>
       </ScrollView>
     );
   }
@@ -190,6 +190,10 @@ var styles = StyleSheet.create({
     flex: 1,
     color: '#000',
     marginLeft: 10
+  },
+  settingButtonTextRight: {
+    color: '#000',
+    marginRight: 10
   },
   logInButton: {
     marginTop: 10,
