435c60ae72f9e9d5c3e506f23f40d975d092600c
Albert Chen <blahoink@gmail.com>
2016-01-12 16:40:04 -0500

    message item cleanup and bigger messages

--- js/chat/components/ios/MessageItem.ios.js
+++ js/chat/components/ios/MessageItem.ios.js
@@ -32,15 +32,24 @@ var MessageItem = React.createClass({
 
   getDefaultProps() {
     return {
-      message: ''
+      message: {},
+      hidePic: false,
+      showName: true
     }
   },
 
   getInitialState() {
     return {
       collapsed: true,
-      wrap: false
+      wrap: false,
+      isMe: this.props.user._id == this.props.message.author._id
     }
+  },
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({
+      isMe: nextProps.user._id == nextProps.message.author._id
+    });
   },
 
   componentDidMount() {
@@ -48,31 +57,30 @@ var MessageItem = React.createClass({
   },
 
   measureMessageBody() {
-    var message = this.props.message;
-    var author = message.author;
-    var user = this.props.user;
-    var isMe = (user._id == author._id);
     setTimeout(() => {
       this.MessageBody.measure((ox, oy, width, height, px, py) => {
-
         if(width >= (constants.width - 80)) {
-          this.setState({
-            wrap: true
-          });
+          this.setState({ wrap: true });
         }
       });
     }, 0);
   },
 
-  render: function() {
-    var message = this.props.message;
-    var author = message.author;
-    var user = this.props.user;
+  toggleCollapsed() {
+    this.setState({ collapsed: !this.state.collapsed });
+  },
 
-    var createDate = new Date(message.created);
+  _renderCreated() {
+    var createDate = new Date(this.props.message.created);
     var nowDate = new Date();
     var diff = nowDate - createDate;
+
+    var weekdayMap = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+    var monthMap = ['Jan', 'Feb', 'Mar', 'Apr', 'May',
+      'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
     var created = '';
+
     if(diff <= ( 1000 * 60 * 60 * 24)) {
       // within a day - only display hours and minutes
       created =
@@ -86,207 +94,137 @@ var MessageItem = React.createClass({
         + ((createDate.getHours() > 11) ? ' PM' : ' AM');
     } else if (diff <= ( 1000 * 60 * 60 * 24 * 7)) {
       // within a week - only display short weekday
-      var weekdayMap = [
-        'Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'
-      ];
       created = weekdayMap[createDate.getDay()];
     } else {
       // outside of a week - display month and day
-      var monthMap = [
-        'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
-        'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
-      ];
       created = monthMap[createDate.getMonth() - 1] + ' ' + createDate.getDate();
     }
+    return created;
+  },
 
-    var messageBodyStyle = {
-      backgroundColor: '#rgba(0,0,0,.2)',
-      paddingTop: 5,
-      paddingBottom: 5,
-      paddingLeft: 8,
-      paddingRight: 8,
-      borderRadius: 14,
-      flexWrap: 'wrap',
-      flex: (this.state.wrap) ? 1 : 0,
-    };
+  _renderImage() {
+    if(this.props.hidePic) return <View style={{height: 5, width: 50}}/>;
 
-    var isMe = (user._id == author._id);
-
-    var image = (this.props.hidePic)
-    ? <View style={{height: 5, width: 35}}/>
-    : <Image
-      source={{ uri: (_.isEmpty(author.image))
-        ? constants.siteurl + '/img/user-profile-icon.png'
-        : resizeImage(author.image, 64, 64).url }}
-      style={ styles.authorImage }
-    />
-
-    var name = (isMe) ? 'Me' : author.displayName;
-
-    var nameTitle = (this.props.showName)
-    ? <Text
-        style={{
-          width: constants.width,
-          flexDirection: 'column',
-          textAlign: (isMe) ? 'right' : 'left',
-          paddingHorizontal: 45,
-          color: 'rgba(0,0,0,.3)'
-        }}
-      >
-        {name}
-      </Text>
-    : <View/>;
-
-    var space = (this.props.hidePic) ? 5 : 0;
+    var authorImageURL = (_.isEmpty(this.props.message.author.image))
+      ? constants.siteurl + '/img/user-profile-icon.png'
+      : resizeImage(this.props.message.author.image, 64, 64).url;
 
     return (
-      <View>
-        { (isMe)
-          ? (
-            <TouchableOpacity
-              activeOpacity={.5}
-              onPress={() => {
-                this.setState({
-                  collapsed: !this.state.collapsed
-                })
-              }}
+      <Image
+        source={{ uri: authorImageURL }}
+        style={ styles.authorImage }
+      />
+    );
+  },
+
+  _renderName() {
+    if(!this.props.showName) return <View />;
+    return (
+      <Text
+        style={[ styles.authorName, {
+          textAlign: (this.state.isMe) ? 'right' : 'left',
+        }]}
+      >
+        { (this.state.isMe) ? 'Me' : this.props.message.author.displayName }
+      </Text>
+    );
+  },
+
+  render() {
+    return (
+      <TouchableOpacity
+        activeOpacity={ .5 }
+        onPress={ this.toggleCollapsed }
+      >
+        <View style={[ styles.container, {
+          alignItems: (this.state.isMe) ? 'flex-end' : 'flex-start',
+          justifyContent: (this.state.isMe) ? 'flex-end' : 'flex-start',
+        }]}>
+          { this._renderName() }
+          <View style={[ styles.messageContainer, {
+            justifyContent: (this.state.isMe) ? 'flex-end' : 'flex-start',
+            marginTop: (this.props.hidePic) ? 8 : 0
+          }]}>
+            { (this.state.isMe) ? <View /> : this._renderImage() }
+            { (this.state.isMe) ? <View /> : <View style={{ width: 10 }} /> }
+            <View
+              style={[ styles.messageBody, {
+                flex: (this.state.wrap) ? 1 : 0,
+                backgroundColor: (this.state.isMe) ? '#2CB673' : '#EEE'
+              }]}
+              ref={ref => { this.MessageBody = ref; }}
             >
-              <View style={{
-                flexDirection: 'column',
-                alignItems: 'flex-end',
-                justifyContent: 'flex-end',
-              }}>
-                { nameTitle }
-                <View style={[styles.containerMe, {marginTop: space}]}>
-                  <View
-                    style={[messageBodyStyle, { backgroundColor: '#2cb673' } ]}
-                    ref={ref => { this.MessageBody = ref; }}
-                  >
-                    <Text style={{
-                      textAlign: 'right',
-                      color: '#fff',
-                      flex: 1,
-                    }}>
-                      { message.body}
-                    </Text>
-                  </View>
-                  <View style={{width: 5}}/>
-                  { image }
-                </View>
-                <Collapsible collapsed={this.state.collapsed} >
-                  <Text style={[styles.myName, {color: '#888'} ]}>
-                    Me · { created }
-                  </Text>
-                </Collapsible>
-              </View>
-            </TouchableOpacity>
-          )
-          : (
-            <TouchableOpacity
-              activeOpacity={.5}
-              onPress={() => {
-                this.setState({
-                  collapsed: !this.state.collapsed
-                })
-              }}
-            >
-              <View style={{
-                flexDirection: 'column',
-                alignItems: 'flex-start',
-                justifyContent: 'flex-start',
-                backgroundColor: 'rgba(0,0,0,0)'
-              }}>
-                {nameTitle}
-                <View style={[styles.container, {marginTop: space}]}>
-                  { image }
-                  <View style={{width: 5}}/>
-                  <View
-                    style={[messageBodyStyle, {backgroundColor: '#eee'} ]}
-                    ref={ref => { this.MessageBody = ref; }}
-                  >
-                    <Text style={{ textAlign: 'left', color: '#333' }}>
-                      { message.body }
-                    </Text>
-                  </View>
-                </View>
-                <Collapsible collapsed={this.state.collapsed} >
-                  <Text style={[styles.authorName, {color: '#888'} ]}>
-                    {author.displayName} · { created }
-                  </Text>
-                </Collapsible>
-              </View>
-            </TouchableOpacity>
-          )
-        }
-      </View>
+              <Text style={[ styles.messageBodyText, {
+                textAlign: (this.state.isMe) ? 'right' : 'left',
+                color: (this.state.isMe) ? '#fff' : '#333',
+              }]}>
+                { this.props.message.body}
+              </Text>
+            </View>
+            { (this.state.isMe) ? <View style={{ width: 10 }} /> : <View /> }
+            { (this.state.isMe) ? this._renderImage() : <View /> }
+          </View>
+          <Collapsible collapsed={ this.state.collapsed } >
+            <Text style={[ styles.collapseText, {
+              marginRight: (this.state.isMe) ? 50 : 0,
+              marginLeft: (this.state.isMe) ? 0 : 50
+            }]}>
+              {(this.state.isMe) ? 'Me' : this.props.message.author.displayName }
+              &nbsp;·&nbsp;
+              { this._renderCreated() }
+            </Text>
+          </Collapsible>
+        </View>
+      </TouchableOpacity>
     );
   }
 });
 
 var styles = StyleSheet.create({
   container: {
-    flexDirection: 'row',
-    justifyContent: 'flex-start',
-    alignItems: 'center',
-    paddingBottom: 0,
-    borderRadius: 2,
-    backgroundColor: 'rgba(0,0,0,0)',
-    width: constants.width - 80
-  },
-  containerMe: {
-    flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-    paddingBottom: 0,
-    borderRadius: 2,
-    backgroundColor: 'rgba(0,0,0,0)',
-    width: constants.width - 80
-  },
-  authorImage: {
-    width: 35,
-    height: 35,
-    borderRadius: 17.5,
+    flexDirection: 'column',
     backgroundColor: 'rgba(0,0,0,0)'
   },
-  messageBody: {
-    backgroundColor: '#fff',
-    paddingTop: 5,
-    paddingBottom: 5,
-    paddingLeft: 5,
-    paddingRight: 5
-  },
-  messageBodyMe: {
-    backgroundColor: '#fff',
-    paddingTop: 5,
-    paddingBottom: 5,
-    paddingLeft: 5,
-    paddingRight: 5
-  },
-  messageTextContainer: {
-    //flexDirection: 'row',
-    //flex: 1
-  },
-  messageText: {
-
-  },
-  arrow: {
-    width: 10,
-    height: 10
-  },
-  myName: {
-    fontSize: 10,
-    color: '#666',
-    marginBottom: 5,
-    marginTop: 0,
-    marginRight: 40
-  },
   authorName: {
-    fontSize: 10,
-    color: '#666',
+    width: constants.width,
+    flexDirection: 'column',
+    paddingHorizontal: 60,
+    color: 'rgba(0,0,0,.3)',
+    fontSize: 15
+  },
+  authorImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: 'rgba(0,0,0,0)'
+  },
+  messageContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingBottom: 0,
+    borderRadius: 2,
+    backgroundColor: 'rgba(0,0,0,0)',
+    width: constants.width - 80,
+  },
+  messageBody: {
+    backgroundColor: '#rgba(0,0,0,.2)',
+    paddingTop: 5,
+    paddingBottom: 5,
+    paddingLeft: 8,
+    paddingRight: 8,
+    borderRadius: 14,
+    flexWrap: 'wrap'
+  },
+  messageBodyText: {
+    flex: 1,
+    fontSize: 17
+  },
+  collapseText: {
+    fontSize: 15,
+    color: '#888',
     marginBottom: 5,
-    marginTop: 0,
-    marginLeft: 40
-  }
+    marginTop: 5,
+  },
 });
 
 module.exports = MessageItem;
