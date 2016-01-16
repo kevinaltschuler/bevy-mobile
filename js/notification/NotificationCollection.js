c8098e9b52e09e6113b958d3b170e272e4f60852
Albert Chen <blahoink@gmail.com>
2016-01-16 16:04:16 -0500

    fix notification sorting

--- js/notification/NotificationCollection.js
+++ js/notification/NotificationCollection.js
@@ -13,7 +13,9 @@ var constants = require('./../constants');
 
 var NotificationCollection = Backbone.Collection.extend({
   model: Notification,
-  //comparator: '-created'
+  comparator: notification => {
+    return -(new Date(notification.get('created')));
+  }
 });
 
 module.exports = NotificationCollection;
